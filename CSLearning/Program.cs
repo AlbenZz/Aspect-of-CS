32ba4d0294d44d8516d0febb128319c93f0f26e5
AlbenZz <834150509@qq.com>
2015-07-01 06:24:12 +0800

    later commit and first get merged with master with changes file which is changed by other branches

--- CSLearning/Program.cs
+++ CSLearning/Program.cs
@@ -10,7 +10,8 @@ namespace CSLearning
     {
         static void Main(string[] args)
         {
-
+            // I'm the branches that modify a file and make a commit before other branches commit changes on the same file
+            // just check out what may happen while commit changes on the file and merge it back to the master brances while there is also other branches doing the same thing.
 
         }
     }
